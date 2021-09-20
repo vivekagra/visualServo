7c976d3895e2f0a036e481e51202a4b0cc6112fd
vivekagra <vivekagr@iitk.ac.in>
2021-09-20 06:56:06 +0530

    PBVS updated

--- PBVS.py
+++ PBVS.py
@@ -33,50 +33,52 @@ class PBVS():
     z_change = 1
 
     def __init__(self):
-        rospy.init_node('Visual Servoing')
+        rospy.init_node('Visual_Servoing')
         rospy.on_shutdown(self.shutdown)
-        vel_pub = rospy.Publisher('cmd_vel', Twist)
+        self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.move_cmd = Twist()
 
         # The maximum distance a target can be from the robot for us to track
-        self.max_z = rospy.get_param("~max_z", 20.0)
+        self.max_z = rospy.get_param("~max_z", 10.0)
         
-        # The goal distance (in meters) to keep between the robot and the marker
-        self.goal_z = rospy.get_param("~goal_z", 0.6)
-        
-        # How far away from the goal distance (in meters) before the robot reacts
-        self.z_threshold = rospy.get_param("~z_threshold", 0.05)
-        
-        # How far away from being centered (x displacement) of the object before the robot reacts (units are meters)
-        self.x_threshold = rospy.get_param("~x_threshold", 0.05)
-        
-        # How much do we weight the goal distance (z) when making a movement
-        self.z_scale = rospy.get_param("~z_scale", 0.5)
 
+        # The goal distance (in meters) to keep between the robot and the marker
+        self.goal_z = rospy.get_param("~goal_z", 1.0)
+        # The goal distance (in meters) to keep between the robot and the marker
+        self.goal_x = rospy.get_param("~goal_x", 320)
+
+
+        # How far away from the goal distance (in meters) before the robot reacts
+        self.z_threshold = rospy.get_param("~z_threshold", 0.5)
+        # How far away from being centered (x displacement) of the object before the robot reacts (units are meters)
+        self.x_threshold = rospy.get_param("~x_threshold", 100)
+        
+
+        # How much do we weight the goal distance (z) when making a movement
+        self.z_scale = rospy.get_param("~z_scale", 0.05)
         # How much do we weight x-displacement when making a movement        
-        self.x_scale = rospy.get_param("~x_scale", 1.0)
+        self.x_scale = rospy.get_param("~x_scale", 0.001)
+
 
         # The maximum rotation speed in radians per second
-        self.max_angular_speed = rospy.get_param("~max_angular_speed", 2.0)
-        
+        self.max_angular_speed = rospy.get_param("~max_angular_speed", 0.8)
         # The minimum rotation speed in radians per second
         self.min_angular_speed = rospy.get_param("~min_angular_speed", 0.5)
 
+
         # The max linear speed in meters per second
         self.max_linear_speed = rospy.get_param("~max_linear_speed", 0.3)
-        
         # The minimum linear speed in meters per second
         self.min_linear_speed = rospy.get_param("~min_linear_speed", 0.1)
 
-        # The goal distance (in meters) to keep between the robot and the marker
-        self.goal_x = rospy.get_param("~goal_x", 0.6)
 
         # Set flag to indicate when the AR marker is visible
         self.target_visible = False
 
          # Wait for the ObjectRecognition topic to become available
-        rospy.loginfo("Waiting for ar_pose_marker topic...")
-        rospy.wait_for_message('ObjectRecognition', PoseArray)
-        rospy.Subscriber('ObjectRecognition', PoseArray, self.set_cmd_vel)
+        rospy.loginfo("Waiting for object_pose topic...")
+        #rospy.wait_for_message('ObjectRecognition', Point)
+        rospy.Subscriber('obj_pos', Point, self.set_cmd_vel)
 
         rospy.loginfo("Object detected. Starting follower...")
         self.update()
@@ -98,22 +100,22 @@ class PBVS():
             return
         
         # pose with resepect to robot frame
-        current_pose = pos.pose.position
+        current_pose = pos
         current_x = current_pose.x 
         current_y = current_pose.y
         current_z = current_pose.z
 
         # Rotate the robot only if the displacement of the target exceeds the threshold
-        if abs(current_x) > self.x_threshold:
+        if abs(current_x-self.goal_x) > self.x_threshold:
             # Set the rotation speed proportional to the displacement of the target
-            speed = current_x * self.x_scale
+            speed = (current_x-self.goal_x) * self.x_scale
             self.move_cmd.angular.z = copysign(max(self.min_angular_speed, min(self.max_angular_speed, abs(speed))), speed)
         else:
             self.move_cmd.angular.z = 0.0
  
         # Now get the linear speed
         if abs(current_z - self.goal_z) > self.z_threshold:
-            speed = (current_z - self.goal_z) * self.x_scale
+            speed = (current_z - self.goal_z) * self.z_scale
             if speed < 0:
                 speed *= 1.5
             self.move_cmd.linear.x = copysign(min(self.max_linear_speed, max(self.min_linear_speed, abs(speed))), speed)
@@ -132,7 +134,7 @@ class PBVS():
             self.vel_pub.publish(self.move_cmd)
             
             # Sleep for 1/self.rate seconds
-            r.sleep()8
+            # rospy.sleep()
     
     def shutdown(self):
         rospy.loginfo("Stopping the Visual Servoing...")
